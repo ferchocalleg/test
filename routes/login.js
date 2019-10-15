0c802ad24a7acc97b90094a8ac41ff9a228a1763
gabofcg <gabelo005@gmail.com>
2019-10-15 15:53:47 -0500

    Eliminar el campo password del payload del token JWT

--- routes/login.js
+++ routes/login.js
@@ -22,7 +22,8 @@ module.exports = function login () {
   return (req, res, next) => {
     verifyPreLoginChallenges(req)
     //models.sequelize.query('SELECT * FROM Users WHERE email = \'' + (req.body.email || '') + '\' AND password = \'' + insecurity.hash(req.body.password || '') + '\' AND deletedAt IS NULL', { model: models.User, plain: true })
-    models.sequelize.query('SELECT * FROM Users wHERE email = :email AND password = :password AND deletedAt IS NULL', { replacements: { email: req.body.email, password: insecurity.hash(req.body.password)}, model: models.User, plain: true })
+    //models.sequelize.query('SELECT * FROM Users wHERE email = :email AND password = :password AND deletedAt IS NULL', { replacements: { email: req.body.email, password: insecurity.hash(req.body.password)}, model: models.User, plain: true })
+    models.sequelize.query('SELECT id,username,email,isAdmin,lastLoginIp,profileImage,totpSecret,isActive,createdAt,updatedAt,deletedAt FROM Users wHERE email = :email AND password = :password AND deletedAt IS NULL', { replacements: { email: req.body.email, password: insecurity.hash(req.body.password)}, model: models.User, plain: true })
       .then((authenticatedUser) => {
         let user = utils.queryResultToJson(authenticatedUser)
         const rememberedEmail = insecurity.userEmailFrom(req)
